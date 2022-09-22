d70286c79ccf7007218360c1d94b46329b6646a8
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-22 19:43:42 +0400

    maven application converted into spring boot application

---
+++ backend/src/test/java/az/blog/BlogApplicationTests.java
@@ -0,0 +1,11 @@
+package az.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BlogApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
