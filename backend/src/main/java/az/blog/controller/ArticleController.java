c1b136d34f8747b95e30d2ca399397af83fafa46
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-23 18:06:59 +0400

    endpoints created for article

--- backend/src/main/java/az/blog/controller/ArticleController.java
+++ backend/src/main/java/az/blog/controller/ArticleController.java
@@ -1,8 +1,32 @@
 package az.blog.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import az.blog.domain.entity.Article;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("/api/v1")
 public class ArticleController {
+
+    @GetMapping(value = "/articles")
+    public ResponseEntity<List<Article>> getAllArticles() {
+        return null;
+    }
+
+    @PostMapping(value = "/article")
+    public ResponseEntity<Void> createArticle() {
+        return null;
+    }
+
+    @PutMapping(value = "/article")
+    public ResponseEntity<Void> updateArticle() {
+        return null;
+    }
+
+    @DeleteMapping(value = "/article")
+    public ResponseEntity<Void> deleteArticle() {
+        return null;
+    }
 
 }
